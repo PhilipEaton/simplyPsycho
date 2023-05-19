1620b2cbfab1a10332979b8e88966efa6dc9bfbb
PhilipEaton <philip.eaton@stockton.edu>
2023-05-19 12:33:17 -0400

    Added item difficulty comparisions to gen.compare.courses() with plotting option.

--- R/generalCompareCourse.R
+++ R/generalCompareCourse.R
@@ -8,6 +8,11 @@
 #' @param makeBoxPlots (Default = FALSE) If TRUE, then bar plots will be made for
 #' all given courses in a single plot.
 #'
+#' @param makeBoxPlots (Default = FALSE) If TRUE, then point plots will be made for
+#' comparing all possible course pairs.
+#'
+#' NOTE: Use the Left and Right arrow in the Plots window to switch between the plots.
+#'
 #' @return Gives the Cohen's d, an interpretation of the Cohen's d, the p-value,
 #' the adjust p-value for multiple comparisions, and an optional barplot.
 #'
@@ -22,7 +27,7 @@
 #' gen.compare.courses(data, method = "bonferroni")
 #' # ... supports makinf boxplots as well
 #' gen.compare.courses(data, method = "bonferroni", makeBoxPlots = TRUE)
-gen.compare.courses <- function (data, makeBoxPlots = FALSE, ...){
+gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FALSE, ...){
   data.list <- list()
   # Build list of graded data from given courses
   if (typeof(data) == "list") {
@@ -40,14 +45,18 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, ...){
   nQ <- ncol(data.list[[1]])
   total.scores <- lapply(data.list, function(x) rowSums(x))
   names(total.scores) <- paste0("Course",temp.names)
-  nCorses <- length(total.scores)
-  results <- as.data.frame(array(NA, dim = c(4,nCorses*(nCorses-1)/2)))
+  nCourses <- length(total.scores)
+  results <- as.data.frame(array(NA, dim = c(4,nCourses*(nCourses-1)/2)))
+  diff.results <- as.data.frame(array(NA, dim = c(2*nCourses, nQ)))
+  colnames(diff.results) <- colnames(data.list[[1]])
   rownames(results) <- c("Cohen's d", "Interpretation", "p-value", "Adjusted p-value")
+  item.diff <- cttDifficulty(data, booted = TRUE)
   nn <- 1
-  for (ii in 1:nCorses) {
-    for (jj in 1:nCorses) {
+  for (ii in 1:nCourses) {
+    for (jj in 1:nCourses) {
       if (jj > ii) {
         colnames(results)[nn] <- paste0(temp.names[ii], " & ",  temp.names[jj])
+        rownames(diff.results)[c(2*nn-1,2*nn)] <- paste0(c("difference.in.dif","error"),  paste0(temp.names[jj], "-",  temp.names[ii]))
         set1 <- total.scores[[ii]]
         n1 <- length(set1)
         mean1 <- mean(set1)
@@ -71,6 +80,23 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, ...){
         # Results
         results[1,nn] <- as.numeric(round(cohend.val,3))
         results[2,nn] <- cohend.int
+        # Diff comparisions
+        diff.results[2*nn-1,] <- item.diff[[jj]][1,] - item.diff[[ii]][1,]
+        diff.results[2*nn,] <- sqrt(item.diff[[jj]][2,]^2 + item.diff[[ii]][2,]^2)
+        # Diff Plots
+        if (makeDiffPlots == TRUE) {
+          error.bar <- function(x, y, upper, lower=upper, length=0.05,...){
+            arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
+          }
+            x <- unlist(diff.results[2*nn-1,])
+            x.err <- unlist(diff.results[2*nn,])
+            xmax <- max(x)
+            if (max(x)<0.25) {xmax <- 0.25} else if (max(x)<0.5) {xmax <- 0.5} else if (max(x)<0.75) {xmax <- 0.75} else {xmax <- 1}
+            plot(x, ylab = paste0("Change in Difficulty: ", paste0(temp.names[jj], "-",  temp.names[ii])), ylim = c(-xmax,xmax))
+            error.bar(1:nQ, x, x.err)
+            abline(h = 0, col = "black")
+          }
+        # advance nn value
         nn = nn+1
       }
     }
@@ -82,7 +108,10 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, ...){
     boxplot(total.scores, ylim = c(0,nQ), ylab = "Total Scores")
   }
   # Return results
-  return(results)
+  thing.return.master <- list()
+  thing.return.master$results <- results
+  thing.return.master$itemDiff.comparision <- diff.results
+  return(thing.return.master)
 
 }
 
