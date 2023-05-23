5ceb0b32924d926e68c34021a3797b8a3bab8cca
PhilipEaton <philip.eaton@stockton.edu>
2023-05-22 20:04:14 -0400

    Added normalized gain and change to compare function. Updated documentation.

--- R/generalCompareCourse.R
+++ R/generalCompareCourse.R
@@ -7,6 +7,8 @@
 #'
 #' @param data MUST BE the output of piql.data.select for multiple courses selected.
 #'
+#' @param nRuns Number of runs to use when calculating error in CTT Difficulty.
+#'
 #' @param makeBoxPlots (Default = FALSE) If TRUE, then bar plots will be made for
 #' all given courses in a single plot.
 #'
@@ -22,10 +24,19 @@
 #'
 #' NOTE: Use the Left and Right arrow in the Viewer window to switch between the plots.
 #'
-#' @param nBins Number of total score bins to use for the Sankey plots. Bins are
+#' @param nBins Number of total score bins to use for the Sankey plots. Bins are roughly
 #' "equal" ranges of total score. ~(number of total scores possible)/nBins
 #'
-#' @return Prints Skaney plots for all possible pairing of the given data.
+#' @param return.gains (Default = FALSE) If FALSE, the gains are not returned for each student.
+#' IF TRUE, then gains are reported for each student.
+#'
+#' @param makeGainsPlots (Default = FALSE) If true, then plots of normalized gain/change versus
+#' "pre" scores are created.
+#'
+#' @return Cohen's d with interpretation, p-value and adjusted p-value of difference in means,
+#' course normalized gain, difference in item difficulty with error. On request
+#' box plots, change in difficulty plots, Sankey plots, and gains versus pre score plots
+#' cal all be created. Sankey plots and gains versus pre score plots require matched data.
 #'
 #' NOTE: Use the Left and Right arrow in the Plots window to switch between the plots.
 #'
@@ -36,9 +47,21 @@
 #' PIQLdata <- pullPIQLdata()
 #' data <- piql.data.select(PIQLdata, course = c(1,3), Matched = TRUE)
 #'
-#' gen.compare.courses(data, makeSankeyPlots = TRUE)
+#' gen.compare.courses(data)
+#' ## Box Plots can be made
+#' gen.compare.courses(data, makeBoxPlots = TRUE)
+#' ## Change in item difficulty pots can be made
+#' gen.compare.courses(data, makeDiffPlots = TRUE)
 #'
-gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FALSE, makeSankeyPlots = FALSE, nBins = 7, nRuns = 100, ...){
+#' ### MATCHED DATA
+#' ## Sankey plots can be made
+#' gen.compare.courses(data, makeSankeyPlots = TRUE)
+#' ## Plots of normalized gain/change versus pre score for each student
+#' gen.compare.courses(data, makeGainsPlots = TRUE)
+#' ## You can get the gain values by setting return.gains = TRUE
+#' gen.compare.courses(data, return.gains = TRUE)
+#'
+gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FALSE, makeSankeyPlots = FALSE, nBins = 7, nRuns = 100, return.gains = FALSE, makeGainsPlots = FALSE, ...){
   data.list <- list()
   # Build list of graded data from given courses
   if (typeof(data) == "list") {
@@ -52,16 +75,22 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FAL
   if (typeof(data) == "double" || length(data.list) == 1) {
     stop("You need to supply more than one course for comparision to be done.")
   }
-  # Get total scores
+  # Number of questions
   nQ <- ncol(data.list[[1]])
+  nS <- nrow(data.list[[1]])
+  # Get total scores from files in the data lists
   total.scores <- lapply(data.list, function(x) rowSums(x))
   names(total.scores) <- paste0("Course",temp.names)
+  # Get number of courses in data
   nCourses <- length(total.scores)
-  results <- as.data.frame(array(NA, dim = c(4,nCourses*(nCourses-1)/2)))
+  # Open storage variables
+  results <- as.data.frame(array(NA, dim = c(5,nCourses*(nCourses-1)/2)))
+  rownames(results) <- c("Cohen's d", "Interpretation", "p-value", "Adjusted p-value", "Course Norm. Gain")
   diff.results <- as.data.frame(array(NA, dim = c(2*nCourses, nQ)))
   colnames(diff.results) <- colnames(data.list[[1]])
-  rownames(results) <- c("Cohen's d", "Interpretation", "p-value", "Adjusted p-value")
   item.diff <- cttDifficulty(data, booted = TRUE, nRuns = nRuns)
+  norm.gain.full <- as.data.frame(array(NA, dim = c(nS,nCourses)))
+  norm.change.full <- as.data.frame(array(NA, dim = c(nS,nCourses)))
   nn <- 1
   for (ii in 1:nCourses) {
     for (jj in 1:nCourses) {
@@ -78,8 +107,7 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FAL
         sd2 <- sd(set2)
         # effect size - Cohen's d and t-test
         s <- sqrt( ((n1-1)*sd1 + (n2-1)*sd2)/(n1+n2-2) )
-        cohend.val <- (mean1-mean2) / s
-        results[3,nn] <- t.test(set1,set2)$p.value
+        cohend.val <- (mean2-mean1) / s
         # Classification
         if (abs(cohend.val) <= 0.01) {cohend.int <- "Very Small"
         } else if (abs(cohend.val) <= 0.20) {cohend.int <- "Small"
@@ -91,6 +119,9 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FAL
         # Results
         results[1,nn] <- as.numeric(round(cohend.val,3))
         results[2,nn] <- cohend.int
+        results[3,nn] <- t.test(set1,set2)$p.value
+        if (nQ != mean1) {results[5,nn] <- (mean2 - mean1)/(nQ-mean1)
+        } else {results[5,nn] <- NA}
         # Diff comparisions
         diff.results[2*nn-1,] <- item.diff[[jj]][1,] - item.diff[[ii]][1,]
         diff.results[2*nn,] <- sqrt(item.diff[[jj]][2,]^2 + item.diff[[ii]][2,]^2)
@@ -108,8 +139,49 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FAL
             abline(h = 0, col = "black")
         }
         if (makeSankeyPlots == TRUE) {
-          general.compare.courses.MakeSankey(set1, set2, nQ, nBins,temp.names, ii, jj)
+          if (data$Matched == TRUE) {
+            general.compare.courses.MakeSankey(set1, set2, nQ, nBins,temp.names, ii, jj)
+          } else {print("Sankey plot require MATCHED data.")}
         }
+        #Student gains
+        if (data$Matched == TRUE) {
+          norm.gain <- numeric(nS)
+          norm.change <- numeric(nS)
+          for (ss in 1:nS) {
+            if (nQ != set1[ss]) {
+              norm.gain[ss] <- (set2[ss]-set1[ss])/(nQ-set1[ss])
+            } else {norm.gain[ss] <- NA}
+            if (nQ != set1[ss] && nQ != set2[ss] && 0 != set2[ss] && 0 != set1[ss]) {
+              if (set2[ss] > set1[ss]) {
+                norm.change[ss] <- (set2[ss]-set1[ss])/(nQ-set1[ss])
+              } else {
+                norm.change[ss] <- (set2[ss]-set1[ss])/(set1[ss])
+              }
+            } else {norm.gain[ss] <- NA}
+          }
+          norm.gain.full[,nn] <- norm.gain
+          norm.change.full[,nn] <- norm.change
+          colnames(norm.gain.full)[nn] <- paste0(temp.names[jj], "-",  temp.names[ii])
+          colnames(norm.change.full)[nn] <- paste0(temp.names[jj], "-",  temp.names[ii])
+          if (makeGainsPlots == TRUE) {
+            # Gain
+            x = set1
+            y = norm.gain
+            ymax = max(y, na.rm = TRUE) + 0.1
+            ymin = min(y, na.rm = TRUE) - 0.1
+            plot(x, y, ylab = paste0("Norm. Gain: ", paste0(temp.names[jj], "-",  temp.names[ii])),
+                 xlab = paste0("Score on ", temp.names[ii]), ylim = c(ymin,ymax))
+            abline(h=0)
+            # Change
+            y = norm.change
+            ymax = max(y, na.rm = TRUE) + 0.1
+            ymin = min(y, na.rm = TRUE) - 0.1
+            plot(x, y, ylab = paste0("Norm. Change: ", paste0(temp.names[jj], "-",  temp.names[ii])),
+                 xlab = paste0("Score on ", temp.names[ii]), ylim = c(-1,1))
+            abline(h=0)
+          }
+        }
+
         # advance nn value
         nn = nn+1
       }
@@ -125,6 +197,10 @@ gen.compare.courses <- function (data, makeBoxPlots = FALSE, makeDiffPlots = FAL
   thing.return.master <- list()
   thing.return.master$results <- results
   thing.return.master$itemDiff.comparision <- diff.results
+  if (data$Matched == TRUE && return.gains == TRUE) {
+    thing.return.master$norm.gain <- norm.gain.full
+    thing.return.master$norm.change <- norm.change.full
+  }
   return(thing.return.master)
 
 }
