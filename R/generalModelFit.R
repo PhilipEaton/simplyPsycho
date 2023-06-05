e953df626c2a0eea8f69c1e1c2ae2849fbc376bc
PhilipEaton <philip.eaton@stockton.edu>
2023-06-05 09:51:53 -0500

    Added bimodal plotting and updated documentation.

--- R/generalModelFit.R
+++ R/generalModelFit.R
@@ -11,8 +11,8 @@
 #'
 #' @param model (Default = "Normal") Model = "Normal", the mean, standard deviation,
 #' skew, kertosis and standard error will be estimated for the given course(s).
-#' Model = "Bimodal" will also report the possible locations of the maximums (means)
-#' assuming a bimodal distribution.
+#' Model = "Bimodal" will also report the possible location of the two mean values
+#' assuming the sample is drawn from a bimodal distribution.
 #' Model = "Johnson" will use a Johnson fit method to fit the data including the skew and
 #' kurtosis.
 #'
@@ -53,6 +53,7 @@
 #' gen.model.TotalScores(data.multiple, makePlot = TRUE)
 #' # Supports bimodal estimations as well
 #' gen.model.TotalScores(data.multiple, model = "Bimodal", makePlot = TRUE)
+
 gen.model.TotalScores <- function(data, model = "Normal", makePlot = FALSE){
   data.list <- list()
 
@@ -72,13 +73,15 @@ gen.model.TotalScores <- function(data, model = "Normal", makePlot = FALSE){
 
   thing.return.master <- list()
   nQ <- ncol(data.list[[1]]) # Get number of questions
+  grid <- seq(0, nQ, length = (nQ * 5) + 1)
   plotting.info.normals <- array(0, dim = c(length(temp.nums), (nQ * 5) + 1))
+  plotting.info.normal.bimodal <- array(0, dim = c(length(temp.nums), 6))
   plotting.info.counts <- array(0, dim = c(length(temp.nums), (nQ+1)))
   params.main <- list()
-  grid <- seq(0, nQ, length = (nQ * 5) + 1)
   for (nn in 1:length(temp.nums)) {
     data <- data.list[[nn]]
     totalScores <- rowSums(data, na.rm = TRUE)
+    nS <- length(totalScores)
     test1 <- stats::shapiro.test(totalScores)$p.value
     test2 <- nortest::lillie.test(totalScores)$p.value
     meanScore <- mean(totalScores)
@@ -105,9 +108,13 @@ gen.model.TotalScores <- function(data, model = "Normal", makePlot = FALSE){
       params.main[[nn]] <- SuppDists::JohnsonFit(totalScores, moment="quant")
     }
     if (model == "Bimodal") {
-      thing <- suppressWarnings(multimode::locmodes(totalScores, mod0=2,display = FALSE))
+      #thing <- suppressWarnings(multimode::locmodes(totalScores, mod0=2,display = FALSE))
+      temp.bimodal.results <- mixtools::normalmixEM(totalScores, maxit = 5000)
+      plotting.info.normal.bimodal [nn,c(1,2)] <- temp.bimodal.results$mu
+      plotting.info.normal.bimodal [nn,c(3,4)] <- temp.bimodal.results$sigma
+      plotting.info.normal.bimodal [nn,c(5,6)] <- temp.bimodal.results$lambda
       thing.return.master[[nn]] <- data.frame(mean = meanScore, stDev = sdScore, std.error = std_mean(totalScores),
-                                              mode1.est = thing$locations[1], mode2.est = thing$locations[3],
+                                              mean1.est = temp.bimodal.results$mu[1], mean2.est = temp.bimodal.results$mu[2],
                                               norm.test.shapiro = test1, norm.test.lillie = test2)
       names(thing.return.master)[nn] <- noquote(paste0("course", temp.names[nn]))
     }
@@ -116,37 +123,53 @@ gen.model.TotalScores <- function(data, model = "Normal", makePlot = FALSE){
   # PLOTTING
   if (makePlot == TRUE) {
     ymax = max(c(max(plotting.info.normals), max(plotting.info.counts)))
-      plot(x = c(0:nQ), y = plotting.info.counts[1,], pch = 16, col = 1,
-           ylim = c(0,1.1*ymax), xaxt='n', ylab = NA, xlab = NA)
-      axis(1, at = 0:nQ, labels = 0:nQ, cex.axis = 1, padj = -1)
-      title(xlab = "Total Scores", line = 2, cex.lab=1.2)
-      title(ylab = "Fraction of Students", line = 2, cex.lab=1.2)
-      if (model == "Normal") {
-        title(main = "Normal Distribution", line = 2, cex.main=1.5)
-        lines(x = grid, y = plotting.info.normals[1,], col = 1)
-      } else if (model == "Bimodal") {
-        title(main = "Bimodal Distribution", line = 2, cex.main=1.5)
-        abline(v = thing.return.master[[1]][c(4,5)], col = 1, lty = c(1,2))
-      } else if (model == "Johnson") {
-        title(main = "Johnson Fit", line = 2, cex.main=1.5)
-        parms <- params.main[[1]]
-        lines(x = grid, y = SuppDists::dJohnson(grid, parms), col = 1)
-      }
-      if (length(temp.nums) > 1) {
-        for (nn in 2:length(temp.nums)) {
-          points(x = c(0:nQ), y = plotting.info.counts[nn,], pch = 16, col = nn)
-          if (model== "Normal") {
-            lines(x = grid, y = plotting.info.normals[nn,], col = nn)
-          } else if (model == "Bimodal") {
-            abline(v = thing.return.master[[nn]][c(4,5)], col = nn, lty = c(1,2))
-          } else if (model == "Johnson") {
-            parms <- params.main[[nn]]
-            lines(x = grid, y = SuppDists::dJohnson(grid, parms), col = nn)
-          }
+    plot(x = c(0:nQ), y = plotting.info.counts[1,], pch = 16, col = 1,
+         ylim = c(0,1.1*ymax), xaxt='n', ylab = NA, xlab = NA)
+    axis(1, at = 0:nQ, labels = 0:nQ, cex.axis = 1, padj = -1)
+    title(xlab = "Total Scores", line = 2, cex.lab=1.2)
+    title(ylab = "Fraction of Students", line = 2, cex.lab=1.2)
+    if (model == "Normal") {
+      title(main = "Normal Distribution", line = 2, cex.main=1.5)
+      lines(x = grid, y = plotting.info.normals[1,], col = 1)
+    } else if (model == "Bimodal") {
+      title(main = "Bimodal Distribution", line = 2, cex.main=1.5)
+      abline(v = thing.return.master[[1]][c(4,5)], col = 1, lty = c(2,3))
+      means <- plotting.info.normal.bimodal[1,c(1,2)]
+      dev <- plotting.info.normal.bimodal[1,c(3,4)]
+      prop <- plotting.info.normal.bimodal[1,c(5,6)]
+      plotting.info.normal.1 <- dnorm(grid, mean = means[1], sd = dev[1])
+      plotting.info.normal.2 <- dnorm(grid, mean = means[2], sd = dev[2])
+      lines(x = grid, y = prop[1]*plotting.info.normal.1 + prop[2]*plotting.info.normal.2, lty = 1)
+      lines(x = grid, y = prop[1]*plotting.info.normal.1, lty = 2)
+      lines(x = grid, y = prop[2]*plotting.info.normal.2, lty = 3)
+    } else if (model == "Johnson") {
+      title(main = "Johnson Fit", line = 2, cex.main=1.5)
+      parms <- params.main[[1]]
+      lines(x = grid, y = SuppDists::dJohnson(grid, parms), col = 1)
+    }
+    if (length(temp.nums) > 1) {
+      for (nn in 2:length(temp.nums)) {
+        points(x = c(0:nQ), y = plotting.info.counts[nn,], pch = 16, col = nn)
+        if (model== "Normal") {
+          lines(x = grid, y = plotting.info.normals[nn,], col = nn)
+        } else if (model == "Bimodal") {
+          abline(v = thing.return.master[[nn]][c(4,5)], lty = c(2,3), col = nn)
+          means <- plotting.info.normal.bimodal[nn,c(1,2)]
+          dev <- plotting.info.normal.bimodal[nn,c(3,4)]
+          prop <- plotting.info.normal.bimodal[nn,c(5,6)]
+          plotting.info.normal.1 <- dnorm(grid, mean = means[1], sd = dev[1])
+          plotting.info.normal.2 <- dnorm(grid, mean = means[2], sd = dev[2])
+          lines(x = grid, y = prop[1]*plotting.info.normal.1 + prop[2]*plotting.info.normal.2, col = nn, lty = 1)
+          lines(x = grid, y = prop[1]*plotting.info.normal.1, col = nn, lty = 2)
+          lines(x = grid, y = prop[2]*plotting.info.normal.2, col = nn, lty = 3)
+        } else if (model == "Johnson") {
+          parms <- params.main[[nn]]
+          lines(x = grid, y = SuppDists::dJohnson(grid, parms), col = nn)
         }
-        legend("topright", inset = 0.01, legend=c(temp.names), col= c(1:length(temp.names)),
-               lty = 1, cex=1, box.lty=0, ncol = length(temp.names))
       }
+      legend("topright", inset = 0.01, legend=c(temp.names), col= c(1:length(temp.names)),
+             lty = 1, cex=1, box.lty=0, ncol = length(temp.names))
+    }
   }
   # return
   return(thing.return.master)
